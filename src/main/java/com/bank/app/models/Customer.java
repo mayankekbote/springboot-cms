2e4904fcd88628bb6aa44d79517eeb17e8738655
Mayank Ekbote <mayankekbote12@gmail.com>
2026-04-25 19:06:04 +0530

    introduced lombok to reduce loc in model class

--- src/main/java/com/bank/app/models/Customer.java
+++ src/main/java/com/bank/app/models/Customer.java
@@ -1,6 +1,9 @@
 package com.bank.app.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
